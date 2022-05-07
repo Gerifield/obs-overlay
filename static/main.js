203c676b56084433d3c767c0937f69105ea4e75d
Gergely Radics <gergo254@gmail.com>
2022-05-07 13:33:03 +0200

    feat: added some animation

--- static/main.js
+++ static/main.js
@@ -1,9 +1,9 @@
 var socket = new WebSocket("ws://127.0.0.1:8080/websocket");
 socket.onmessage = function (event) {
-    var el = document.getElementById("field1");
-    el.innerText = event.data;
-    el.style.visibility = "visible";
+    var textField1 = document.getElementById("textField1");
+    textField1.innerText = event.data;
+    textField1.classList.add("fadeIn", "animate__animated", "animate__bounce");
     setTimeout(function () {
-        el.style.visibility = "hidden";
+        textField1.classList.remove("fadeIn", "animate__animated", "animate__bounce");
     }, 3000);
 };
--- static/main.ts
+++ static/main.ts
@@ -1,10 +1,11 @@
 let socket = new WebSocket("ws://127.0.0.1:8080/websocket");
+
 socket.onmessage = event => {
-    let el = document.getElementById("field1");
-    el.innerText = event.data
-    el.style.visibility="visible";
+    let textField1 = document.getElementById("textField1");
+    textField1.innerText = event.data;
+    textField1.classList.add("fadeIn", "animate__animated", "animate__bounce");
 
     setTimeout(() => {
-        el.style.visibility="hidden";
+        textField1.classList.remove("fadeIn", "animate__animated", "animate__bounce");
     }, 3000);
 };
