b13affc5ff20f364cbd87712f6b79eb12c1daf0b
Gergely Radics <gergo254@gmail.com>
2022-05-07 13:36:28 +0200

    feat: allow html content

--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 var socket = new WebSocket("ws://127.0.0.1:8080/websocket");
 socket.onmessage = function (event) {
     var textField1 = document.getElementById("textField1");
-    textField1.innerText = event.data;
+    textField1.innerHTML = event.data;
     textField1.classList.add("fadeIn", "animate__animated", "animate__bounce");
     setTimeout(function () {
         textField1.classList.remove("fadeIn", "animate__animated", "animate__bounce");
--- static/main.ts
+++ static/main.ts
@@ -2,7 +2,7 @@ let socket = new WebSocket("ws://127.0.0.1:8080/websocket");
 
 socket.onmessage = event => {
     let textField1 = document.getElementById("textField1");
-    textField1.innerText = event.data;
+    textField1.innerHTML = event.data;
     textField1.classList.add("fadeIn", "animate__animated", "animate__bounce");
 
     setTimeout(() => {
